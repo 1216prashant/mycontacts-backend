9b651a0d2f81dd223530d46a1f20961e96b9ae5b
Prashant Deshpande <prashant.deshpande@lyveglobal.com>
2023-07-05 12:41:10 +0530

    cors added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
@@ -12,7 +13,7 @@ app.listen(PORT,()=>{
 })
 
 app.use(express.json()); // to use json format in response
-app.use("/api/contacts", require("./routes/contactRoutes")) // routes for contact
-app.use("/api/users", require("./routes/userRoutes")) // routes for user registration & login
+app.use(cors("/api/contacts", require("./routes/contactRoutes"))) // routes for contact
+app.use(cors("/api/users", require("./routes/userRoutes"))) // routes for user registration & login
 app.use(errorHandler) // for error handling of the API's
 
