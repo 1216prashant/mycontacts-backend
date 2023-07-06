cc89151cbdce7f6b111540d35a3d57763ccedf32
Prashant Deshpande <prashant.deshpande@lyveglobal.com>
2023-07-06 13:31:20 +0530

    success message updated

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ const ContactModel = require("../models/contactModel");
 //@route GET /api/contacts/
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await ContactModel.find({user_id: req.user.id});
+  const contacts = await ContactModel.find({ user_id: req.user.id });
   //res.status(200).json({ message: "Get All Contacts" });
   res.status(200).json(contacts);
 });
@@ -23,9 +23,9 @@ const postContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
-    user_id: req.user.id
+    user_id: req.user.id,
   });
-  res.status(201).json(contact);
+  res.status(201).json({ message: "Create Success", contact });
 });
 
 //@desc Get Single Contact
@@ -49,18 +49,19 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact Not Found");
   }
 
-
-if(contact.user_id.toString()  !== req.user.id){
-  res.status(403);
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
     throw new Error("User dont have permission to update other user contact");
-}
+  }
 
   const updatedContact = await ContactModel.findByIdAndUpdate(
     req.params.id,
     req.body,
     { new: true }
   );
-  res.status(200).json(updatedContact);
+  res
+    .status(200)
+    .json({ message: "Update Success", updateContact: updatedContact });
 });
 
 //@desc Delete a Contact
@@ -72,13 +73,13 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact Not Found");
   }
-  if(contact.user_id.toString()  !== req.user.id){
+  if (contact.user_id.toString() !== req.user.id) {
     res.status(403);
-      throw new Error("User dont have permission to delete other user contact");
+    throw new Error("User dont have permission to delete other user contact");
   }
 
-  await ContactModel.deleteOne({_id:req.params.id})
-  res.status(200).json(contact);
+  await ContactModel.deleteOne({ _id: req.params.id });
+  res.status(200).json({ message: "Delete Success", contact: contact });
 });
 
 module.exports = {
